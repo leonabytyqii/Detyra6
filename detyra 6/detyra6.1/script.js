fd703d0ddf0a103353952e304f96d5bd1f3f32e6
leonabytyqii <lb73150@ubt-uni.net>
2025-12-05 15:28:58 +0100

    js

---
+++ detyra 6/detyra6.1/script.js
@@ -0,0 +1,6 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // parandalon refresh-in e faqes
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+});
